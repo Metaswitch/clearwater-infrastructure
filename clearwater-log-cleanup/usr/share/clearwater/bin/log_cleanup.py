1ab8c33df60b512d9b2f28a647c7d188ddc12b4c
Rob Day <Robert.Day@metaswitch.com>
2013-07-22 14:05:38 +0100

    Move logging cleanup script into separate package

---
+++ clearwater-log-cleanup/usr/share/clearwater/bin/log_cleanup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import os
+from os.path import isfile, getsize, exists
+import argparse
+
+def get_logs(dir, prefix):
+    """Return a list of all files in dir which start with prefix"""
+    if exists(dir):
+        return [dir+i for i in os.listdir(dir)
+                    if (isfile(dir+i) and i.startswith(prefix))]
+    else:
+        return []
+
+def total(logfiles):
+    """Return the total filesize, in bytes, of all files in logfiles"""
+    return sum([getsize(f) for f in logfiles])
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Delete the oldest files in DIRECTORY beginning with PREFIX until the total size is under MAXSIZE.')
+	parser.add_argument('directory')
+	parser.add_argument('prefix')
+	parser.add_argument('maxsize', type=int)
+	args = parser.parse_args()
+
+        # Define the log files to monitor
+	logs = get_logs(args.directory, args.prefix)
+	size_to_delete = total(logs) - args.maxsize
+	for logfile in sorted(logs):
+	    if (size_to_delete > 0):
+	        size_to_delete -= getsize(logfile)
+	        os.unlink(logfile)
