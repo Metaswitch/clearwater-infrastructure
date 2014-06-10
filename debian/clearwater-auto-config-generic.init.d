b37c3ec953e8b0d6ec7589d0bb7a189f8fe98a57
Eleanor Merry <eleanor.merry@metaswitch.com>
2014-06-10 17:08:50 +0000

    Updates after review

--- debian/clearwater-auto-config-generic.init.d
+++ debian/clearwater-auto-config-generic.init.d
@@ -45,6 +45,7 @@
 # X-Start-Before:    clearwater-infrastructure bono sprout homer homestead ellis restund
 ### END INIT INFO
 
+# Changes in this command should be replicated in clearwater-auto-config-aws.init.d
 do_auto_config()
 {
   config=/etc/clearwater/config
