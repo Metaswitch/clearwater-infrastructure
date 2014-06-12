1e4d1eb0589eea3bc535886403cadcfd93f24995
Eleanor Merry <eleanor.merry@metaswitch.com>
2014-06-12 14:12:41 +0000

    Add new file based on count not size

--- clearwater-log-cleanup/usr/share/clearwater/bin/log_cleanup.py
+++ clearwater-log-cleanup/usr/share/clearwater/bin/log_cleanup.py
@@ -3,21 +3,20 @@ import os
 from os.path import isfile, getsize, exists
 import argparse
 
-def get_logs(dir, prefix, suffix):
-    """Return a list of all files in dir which start with prefix or end with suffix"""
+def get_logs(dir, prefix):
+    """Return a list of all files in dir which start with prefix"""
     return [dir+'/'+i for i in os.listdir(dir)
-                if ((isfile(dir+'/'+i)) and (((prefix != "") and (i.startswith(prefix))) or ((suffix != "") and (i.endswith(suffix)))))]
+                if (isfile(dir+'/'+i) and i.startswith(prefix))]
 
 def total(logfiles):
     """Return the total filesize, in bytes, of all files in logfiles"""
     return sum([getsize(f) for f in logfiles])
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Delete the oldest files in DIRECTORY beginning with PREFIX and ending with SUFFIX (if set) until the total size is under MAXSIZE.')
+	parser = argparse.ArgumentParser(description='Delete the oldest files in DIRECTORY beginning with PREFIX until the total size is under MAXSIZE.')
 	parser.add_argument('directory')
 	parser.add_argument('prefix')
 	parser.add_argument('maxsize', type=int)
-	parser.add_argument('--suffix', default="")
 	args = parser.parse_args()
 
 	logs = get_logs(args.directory, args.prefix)
