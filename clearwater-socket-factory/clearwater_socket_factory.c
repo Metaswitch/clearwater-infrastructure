82d1d21e117ca4d64d3d77fb06120ade4f941e0f
Alex Hockey <alex.hockey@metaswitch.com>
2014-12-11 15:04:48 +0000

    Make the socket factory return connected sockets

--- clearwater-socket-factory/clearwater_socket_factory.c
+++ clearwater-socket-factory/clearwater_socket_factory.c
@@ -5,9 +5,104 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <netdb.h>
+#include <errno.h>
 
 #define SOCKET_PATH "/tmp/clearwater_mgmt_namespace_socket"
 #define MAX_PENDING 5
+
+int get_shared_socket(char* target)
+{
+  int rc = 0;
+  struct addrinfo hints;
+  struct addrinfo* addrs = NULL;
+  struct addrinfo* p;
+
+  char* sep = strrchr(target, ':');
+
+  if (sep == NULL)
+  {
+    fprintf(stderr, "  Bad target: %s\n", target);
+    rc = -1;
+    goto EXIT_LABEL;
+  }
+
+  *sep = 0;
+  char* host = target;
+  char* port = sep + 1;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo(host, port, &hints, &addrs) != 0)
+  {
+    fprintf(stderr, "  Could not resolve %s: %s\n", host, strerror(errno));
+    rc = -2;
+    goto EXIT_LABEL;
+  }
+
+  printf("  Attempting to connect\n");
+  rc = -3;
+
+  for (p = addrs; p != NULL; p = p->ai_next)
+  {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+      perror("Could not create shared socket");
+      rc = -3;
+      goto EXIT_LABEL;
+    }
+
+    /* Wait 30s for the connection to come up */
+    struct timeval timeout;
+    timeout.tv_sec = 30;
+    timeout.tv_usec = 0;
+
+    if (setsockopt(sock,
+                   SOL_SOCKET,
+                   SO_SNDTIMEO,
+                   (char *)&timeout,
+                   sizeof(timeout)) < 0)
+    {
+      perror("  Failed to set timeout on shared socket");
+      rc = -4;
+      close(sock);
+      goto EXIT_LABEL;
+    }
+
+    if (connect(sock, p->ai_addr, p->ai_addrlen) < 0)
+    {
+      char str[INET_ADDRSTRLEN];
+      inet_ntop(AF_INET,
+                &(((struct sockaddr_in*)(p->ai_addr))->sin_addr),
+                str,
+                sizeof(str));
+      fprintf(stderr, "    Could not connect to %s: %s\n", str, strerror(errno));
+      close(sock);
+    }
+    else
+    {
+      /* Connection successful.*/
+      rc = sock;
+      break;
+    }
+  }
+
+  if (rc < 0)
+  {
+    fprintf(stderr, "  All connections failed\n");
+    goto EXIT_LABEL;
+  }
+
+  printf("  Shared socket connected\n");
+
+EXIT_LABEL:
+
+  freeaddrinfo(addrs);
+  return rc;
+}
 
 int send_file_descriptor(int socket, int fd_to_send)
 {
@@ -43,10 +138,69 @@ int send_file_descriptor(int socket, int fd_to_send)
 }
 
 
+void process_one_request(int listen_socket)
+{
+  int client_sock = accept(listen_socket, NULL, NULL);
+  printf("Received new request\n");
+
+  /*
+   * The client should now tell us the address it wants to connect to in the
+   * form `<host>:<port>`
+   *
+   * Wait at most 1s for this.
+   */
+  struct timeval timeout;
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
+
+  if (setsockopt(client_sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+  {
+    perror("  Could not set timeout on client socket");
+    close(client_sock);
+    return;
+  }
+
+  char buf[1024];
+  size_t len = recv(client_sock, buf, sizeof(buf), 0);
+
+  if (len <= 0)
+  {
+    perror("  Could not read target address");
+    close(client_sock);
+    return;
+  }
+  else if (len >= sizeof(buf))
+  {
+    fprintf(stderr, "  Target address is too long (%lu)\n", len);
+    close(client_sock);
+    return;
+  }
+
+  /* NUL temrinate the target string */
+  buf[len] = 0;
+  printf("  Asked to connect to %s\n", buf);
+
+  int shared_sock = get_shared_socket(buf);
+
+  if(shared_sock < 0)
+  {
+    close(client_sock);
+    return;
+  }
+
+  send_file_descriptor(client_sock, shared_sock);
+
+  close(shared_sock);
+  close(client_sock);
+}
+
+
 int create_server()
 {
   struct sockaddr_un addr;
   int fd;
+
+  printf("Starting server\n");
 
   if ((fd = socket(AF_LOCAL, SOCK_STREAM, 0)) < 0)
   {
@@ -78,15 +232,11 @@ int create_server()
     return -1;
   }
 
+  printf("Listening for requests\n");
+
   for (;;)
   {
-    int client_sock = accept(fd, NULL, NULL);
-
-    int shared_sock = socket(AF_INET,SOCK_STREAM,0);
-    send_file_descriptor(client_sock, shared_sock);
-
-    close(shared_sock);
-    close(client_sock);
+    process_one_request(fd);
   }
 
   return fd;
--- clearwater-socket-factory/test_client.c
+++ clearwater-socket-factory/test_client.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
 #define SOCKET_PATH "/tmp/clearwater_mgmt_namespace_socket"
 
@@ -51,10 +52,12 @@ static int recv_file_descriptor(int socket)
   return -1;
 }
 
-int get_magic_socket()
+int get_magic_socket(char* host, char* port)
 {
   struct sockaddr_un addr;
   int fd;
+
+  printf("Get socket to %s:%s\n", host, port);
 
   if ((fd = socket(AF_LOCAL, SOCK_STREAM, 0)) < 0)
   {
@@ -74,6 +77,15 @@ int get_magic_socket()
     return -1;
   }
 
+  char target[1024];
+  sprintf(target, "%s:%s", host, port);
+
+  if (send(fd, target, strlen(target), 0) < 0)
+  {
+    fprintf(stderr, "Error sending target '%s' to server: %s", target, strerror(errno));
+    return -2;
+  }
+
   int magic_sock = recv_file_descriptor(fd);
 
   return magic_sock;
@@ -81,38 +93,15 @@ int get_magic_socket()
 
 int main(int argc, char** argv)
 {
-  int port;
-  char* host;
-
-  if (argc < 3) {
+  if (argc < 3)
+  {
     fprintf(stderr, "Usage is: %s host port\n", argv[0]);
     return -1;
   }
 
-  port = atoi(argv[2]);
+  int sock = get_magic_socket(argv[1], argv[2]);
 
-  if ((port <= 0) || (port > 0xffff)) {
-    fprintf(stderr, "Invalid port: %s", argv[1]);
-    return -2;
-  }
-
-  host = argv[1];
-  printf("Connect to %s:%d\n", host, port);
-
-  int sock = get_magic_socket();
-
-  struct sockaddr_in servaddr;
-  bzero(&servaddr, sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(port);
-  inet_pton(AF_INET, host, &(servaddr.sin_addr));
-
-  if (connect(sock, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-    perror("Could not connect to TCP server");
-    return -3;
-  }
-
-  char* msg = "Hello there";
+  char* msg = "Hello there\n";
   printf("Connected. Sending message\n");
   send(sock, msg, strlen(msg), 0);
 
