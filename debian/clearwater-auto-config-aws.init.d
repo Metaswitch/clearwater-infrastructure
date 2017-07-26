6a4cfb723b421b0c9033bf532e608d1007c5a74e
Richard Whitehouse <richard.whitehouse@metaswitch.com>
2017-07-26 18:46:39 +0000

    Update AIO nodes to not require SRV records

--- debian/clearwater-auto-config-aws.init.d
+++ debian/clearwater-auto-config-aws.init.d
@@ -20,42 +20,21 @@
 # X-Start-Before:    clearwater-infrastructure bono sprout homer homestead ellis restund
 ### END INIT INFO
 
+. /usr/share/clearwater-auto-config/bin/init-functions
+
 # Changes in this command should be replicated in clearwater-auto-config-*.init.d
 do_auto_config()
 {
-  mkdir -p /etc/clearwater
-
-  local_config=/etc/clearwater/local_config
-  shared_config=/etc/clearwater/shared_config
-
   local_ip=$(wget -qO - http://169.254.169.254/latest/meta-data/local-ipv4)
   public_ip=$(wget -qO - http://169.254.169.254/latest/meta-data/public-ipv4)
   public_hostname=$(wget -qO - http://169.254.169.254/latest/meta-data/public-hostname)
 
-  sed -e 's/^local_ip=.*$/local_ip='$local_ip'/g
-          s/^public_ip=.*$/public_ip='$public_ip'/g
-          s/^etcd_cluster=.*$/etcd_cluster='$local_ip'/g
-          s/^public_hostname=.*$/public_hostname='$public_hostname'/g' -i $local_config
-
-  sed -e 's/^sprout_hostname=.*$/sprout_hostname='$public_hostname'/g
-          s/^xdms_hostname=.*$/xdms_hostname='$public_hostname':7888/g
-          s/^hs_hostname=.*$/hs_hostname='$public_hostname':8888/g
-          s/^hs_provisioning_hostname=.*$/hs_provisioning_hostname='$public_hostname':8889/g
-          s/^sprout_registration_store=.*$/sprout_registration_store='$public_hostname'/g
-          s/^upstream_hostname=.*$/upstream_hostname='$public_hostname'/g' -i $shared_config
-
-  # Sprout will replace the cluster-settings file with something appropriate when it starts
-  rm -f /etc/clearwater/cluster_settings
-
-  # Ensure that we start a clean cluster, so that cloned AIO nodes etc. boot cleanly
-  rm -rf /var/lib/clearwater-etcd/*
-
-  # Set up DNS for the S-CSCF
-  grep -v ' #+scscf.aio$' /etc/hosts > /tmp/hosts.$$
-  echo $local_ip scscf.$public_hostname '#+scscf.aio'>> /tmp/hosts.$$
-  mv /tmp/hosts.$$ /etc/hosts
-
-  service dnsmasq restart
+  write_config \
+    $local_ip \
+    $public_ip \
+    $public_hostname \
+    $public_hostname \
+    $public_hostname
 }
 
 case "$1" in
