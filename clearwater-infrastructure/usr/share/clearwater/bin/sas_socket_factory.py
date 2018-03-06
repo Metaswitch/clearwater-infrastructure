7f3daa6020dc6098bcf11675623e5d5bddf6baa1
Martin Fidczuk <martin.fidczuk@metaswitch.com>
2018-03-06 11:11:25 +0000

    Updated os.system commands

--- clearwater-infrastructure/usr/share/clearwater/bin/sas_socket_factory.py
+++ clearwater-infrastructure/usr/share/clearwater/bin/sas_socket_factory.py
@@ -20,16 +20,16 @@ SAS_CONFIG_FILE = "/etc/clearwater/sas.json"
 SIGNALING_CFG_DIR = "/etc/clearwater-socket-factory/signaling.d"
 MANAGEMENT_CFG_DIR = "/etc/clearwater-socket-factory/management.d"
 
-os.system("mkdir -p " + SIGNALING_CFG_DIR)
-os.system("mkdir -p " + MANAGEMENT_CFG_DIR)
+os.makedirs(SIGNALING_CFG_DIR)
+os.makedirs(MANAGEMENT_CFG_DIR)
 
 sas_use_signaling_namespace = sys.argv[1]
 if sas_use_signaling_namespace is "Y":
     file_to_write = SIGNALING_CFG_DIR + "/clearwater-infrastructure"
-    os.system("rm -f " + MANAGEMENT_CFG_DIR + "/clearwater-infrastructure")
+    os.remove(MANAGEMENT_CFG_DIR + "/clearwater-infrastructure")
 else:
     file_to_write = MANAGEMENT_CFG_DIR + "/clearwater-infrastructure"
-    os.system("rm -f " + SIGNALING_CFG_DIR + "/clearwater-infrastructure")
+    os.remove(SIGNALING_CFG_DIR + "/clearwater-infrastructure")
 
 if os.path.isfile(SAS_CONFIG_FILE):
     with open(SAS_CONFIG_FILE, 'r') as config_file:
@@ -40,7 +40,7 @@ if os.path.isfile(SAS_CONFIG_FILE):
             for sas_server_dict in sas_servers_list:
                 whitelist_file.write(sas_server_dict['ip'] + '\n')
     else:
-        os.system("rm -f " + SIGNALING_CFG_DIR + "/clearwater-infrastructure")
-        os.system("rm -f " + MANAGEMENT_CFG_DIR + "/clearwater-infrastructure")
+        os.remove(SIGNALING_CFG_DIR + "/clearwater-infrastructure")
+        os.remove(MANAGEMENT_CFG_DIR + "/clearwater-infrastructure")
 else:
     raise IOError("File is missing or inaccessible: %s", SAS_CONFIG_FILE)
